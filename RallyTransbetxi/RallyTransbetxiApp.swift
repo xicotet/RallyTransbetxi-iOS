d6b0a46b0bd0d389ec1a3907930d5e3fb3167ce5
xicotet <pablocanofranch@gmail.com>
2024-07-11 13:12:48 +0200

    Initial Commit

---
+++ RallyTransbetxi/RallyTransbetxiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RallyTransbetxiApp.swift
+//  RallyTransbetxi
+//
+//  Created by Pablo on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct RallyTransbetxiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
